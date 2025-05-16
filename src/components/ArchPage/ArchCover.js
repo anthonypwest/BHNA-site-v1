54ffc0bb701c50239b1c2a6ca888ab487f0ca3b4
Anthony West <56272592+anthonypwest@users.noreply.github.com>
2025-05-15 17:20:27 -0700

    Create ArchCover.js

---
+++ src/components/ArchPage/ArchCover.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { useArchQuery } from "../../hooks/useArchQuery"
+
+import { getImage } from "gatsby-plugin-image"
+import { StyledImg, ParagraphIntro, Wrapper } from "./ArchCover.styles"
+
+const ArchPage = () => {
+   const {
+    wpPage: { ACF_ArchStyles: data}
+   } = useArchQuery();
+   
+   const imgData = getImage(data.archImage2.localFile); 
+
+   return (
+           <Wrapper>
+            
+            <ParagraphIntro>{data.description}</ParagraphIntro>
+            <StyledImg image={imgData} alt="BH Arch Example" />
+            </Wrapper>
+            
+    )
+}
+export default ArchPage

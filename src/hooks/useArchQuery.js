44e2033fddbb9223bb177cc569644824f0d41593
Anthony West <56272592+anthonypwest@users.noreply.github.com>
2025-05-17 22:35:44 -0700

    Update useArchQuery.js

--- src/hooks/useArchQuery.js
+++ src/hooks/useArchQuery.js
@@ -35,9 +35,80 @@ export const useArchQuery = () => {
           }
         }
       }
+      archImage3 {
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 1080) {
+              base64
+              tracedSVG
+              srcWebp
+              srcSetWebp
+              originalImg
+              originalName
+            }
+            gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+          }
+        }
+      }
+      archImage4 {
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 1080) {
+              base64
+              tracedSVG
+              srcWebp
+              srcSetWebp
+              originalImg
+              originalName
+            }
+            gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+          }
+        }
+      }
+      archImage5 {
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 1080) {
+              base64
+              tracedSVG
+              srcWebp
+              srcSetWebp
+              originalImg
+              originalName
+            }
+            gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+          }
+        }
+      }
+      archImage6 {
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 1080) {
+              base64
+              tracedSVG
+              srcWebp
+              srcSetWebp
+              originalImg
+              originalName
+            }
+            gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+          }
+        }
+      }
       archStylesText1
-      mainSection
-      archLink
+      mainSection1
+      mainSection2
+      mainSection3
+      mainSection4
+      mainSection5
+      mainSection6
+      archLink1
+      archLink2
+      archLink3
+      archLink4
+      archLink5
+      archLink6
+      archLink7
       
     }
   }

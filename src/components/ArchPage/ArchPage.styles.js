7addc026e88d193cea9e1dd4ba160d6a480bd5c2
Anthony West <56272592+anthonypwest@users.noreply.github.com>
2025-05-19 17:14:56 -0700

    Update ArchPage.styles.js

--- src/components/ArchPage/ArchPage.styles.js
+++ src/components/ArchPage/ArchPage.styles.js
@@ -58,7 +58,7 @@ export const SectionCopyRed = styled.p`
   background-color: ${props => props.theme.colors.main3};
   color: #fff;
   width: 100%;
-  padding: 1.0rem 3.0rem 1.0rem 7.0rem;
+  padding: 0.8rem 4.0rem 0.8rem 1.5rem;;
 `
 
 export const SectionImg = styled.div`

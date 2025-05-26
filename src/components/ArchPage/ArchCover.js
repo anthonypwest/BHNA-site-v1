b105cf3656432f6839fcf91193b2626256f6aa93
Anthony West <56272592+anthonypwest@users.noreply.github.com>
2025-05-26 08:06:34 -0700

    Update ArchCover.js

--- src/components/ArchPage/ArchCover.js
+++ src/components/ArchPage/ArchCover.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { useArchQuery } from "../../hooks/useArchQuery"
-import parse from 'html-react-parser'
 
 import { getImage } from "gatsby-plugin-image"
 import { StyledImg, ParagraphIntro, Wrapper } from "../LandmarkCover/LandmarkCover.styles"
@@ -15,7 +14,7 @@ const ArchPage = () => {
    return (
            <Wrapper>
             
-            <ParagraphIntro>{parse(`${data.mainSection1}`)}</ParagraphIntro>
+            <ParagraphIntro>{data.mainSection1}</ParagraphIntro>
             <StyledImg image={imgData} alt="Composite" />
             </Wrapper>
             

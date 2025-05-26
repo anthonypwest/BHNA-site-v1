ab2423cb1536a80f5e9a18e18dfc6f5c2cf28470
Anthony West <56272592+anthonypwest@users.noreply.github.com>
2025-05-26 07:56:46 -0700

    Update ArchCover.js

--- src/components/ArchPage/ArchCover.js
+++ src/components/ArchPage/ArchCover.js
@@ -1,25 +1,24 @@
 import React from "react"
 import { useArchQuery } from "../../hooks/useArchQuery"
-import parse from 'html-react-parser'
 
 import { getImage } from "gatsby-plugin-image"
-import { StyledImg, ParagraphIntro, Wrapper } from "./ArchCover.styles"
+import { StyledImg, ParagraphIntro, Wrapper } from "../LandmarkCover/LandmarkCover.styles"
 
 const ArchPage = () => {
    const {
     wpPage: { ACF_ArchStyles: data}
    } = useArchQuery();
    
-   const imgData1 = getImage(data.archImage1.localFile); 
+   const imgData = getImage(data.archImage1.localFile); 
 
    return (
            <Wrapper>
             
             <ParagraphIntro>{parse(`${data.mainSection1}`)}</ParagraphIntro>
-            <StyledImg image={imgData1} alt="Composite" />
+            <StyledImg image={imgData} alt="Composite" />
             </Wrapper>
-
-
+            
     )
 }
 export default ArchPage
+
